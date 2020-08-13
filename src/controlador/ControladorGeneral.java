09516be3cb6ec8ad37591ed5086296a939964522
Javier Moraga <javier_moragav@hotmail.com>
2020-08-13 19:24:47 -0400

    TU04 - HU04: Definir los limites de la pantalla para la frase formateada
    Al llegar al limite del lienzo, la frase hará un salto de linea para no salirse al escribir.

--- src/controlador/ControladorGeneral.java
+++ src/controlador/ControladorGeneral.java
@@ -19,10 +19,11 @@ public class ControladorGeneral {
     public AnchorPane lienzo;
     public Label fraseFormat;
 
-    private String frase;
+    private String frase="";
     private String expresion;
 
     public void initialize(){
+        fraseFormat.setWrapText(true);
         simbolosTP.getStyleClass().set(0,"titled-pane");
         simbolosTP.setExpanded(false);
         opcionesTP.getStyleClass().set(0,"titled-pane");
@@ -40,8 +41,19 @@ public class ControladorGeneral {
     public void ingresarFrase(KeyEvent event){
         String cRaw = event.getCharacter();
         char c = cRaw.charAt(0);
-        if(Character.isLetter(c) || Character.isWhitespace(c) || Character.isISOControl(c)){
+        if(Character.isLetter(c) || Character.isWhitespace(c)){
             fraseTF.setEditable(true);
+            frase=frase+c;
+            System.out.println("frase: "+frase);
+        }else if(Character.isISOControl(c)){
+            fraseTF.setEditable(true);
+            String[] fraseSplit = frase.split("");
+            int i=0;
+            frase="";
+            while(i<fraseSplit.length-1){
+                frase=frase+fraseSplit[i];
+                i++;
+            }
         }else{
             fraseTF.setEditable(false);
         }
@@ -50,42 +62,43 @@ public class ControladorGeneral {
     public void simbolo1() throws IOException {
         String texto = fraseTF.getText();
         fraseTF.setText(texto + "»");
+        frase=frase+ "»";
         formatear();
     }
-
     public void simbolo2() throws IOException {
         String texto = fraseTF.getText();
         fraseTF.setText(texto + "«");
+        frase=frase+"«";
         formatear();
     }
-
     public void simbolo3() throws IOException {
         String texto = fraseTF.getText();
         fraseTF.setText(texto + "\"");
+        frase=frase+"\"";
         formatear();
     }
-
     public void simbolo4() throws IOException {
         String texto = fraseTF.getText();
         fraseTF.setText(texto + "\"");
+        frase=frase+"\"";
         formatear();
     }
-
     public void simbolo5() throws IOException {
         String texto = fraseTF.getText();
         fraseTF.setText(texto + "'");
+        frase=frase+"'";
         formatear();
     }
-
     public void simbolo6() throws IOException {
         String texto = fraseTF.getText();
         fraseTF.setText(texto + "'");
+        frase=frase+"'";
         formatear();
     }
-
     public void simbolo7() throws IOException {
         String texto = fraseTF.getText();
         fraseTF.setText(texto + "...");
+        frase=frase+"...";
         formatear();
     }
 
@@ -95,10 +108,34 @@ public class ControladorGeneral {
      * @throws IOException
      */
     public void formatear() throws IOException {
+        double posX,ancho;
+        posX=fraseFormat.getLayoutX();
+        ancho=fraseFormat.getWidth();
+        double limite = 547.0-posX-20.0;
         CargadorFuentes fuenteMaster= new CargadorFuentes();
-        String frase= fraseTF.getText();
         fraseFormat.setFont(fuenteMaster.manuscrita);
-        fraseFormat.setText(frase);
-        System.out.println("done "+frase);
+        System.out.println("Label X: "+fraseFormat.getLayoutX()+" Label Y: "+fraseFormat.getLayoutY());
+        System.out.println("width: "+fraseFormat.getWidth()+"  height: "+fraseFormat.getHeight());
+        System.out.println("limite: "+limite);
+        if (ancho >=limite){
+            System.out.println("mucho ancho");
+            String[] fraseSplit=frase.split("");
+            frase="";
+            int i=0;
+            while(i<fraseSplit.length-2){
+                frase=frase+fraseSplit[i];
+                i++;
+            }
+            frase=frase+"\n"+fraseSplit[i]+fraseSplit[i+1];
+            fraseFormat.setText(frase);
+            System.out.println("done "+frase);
+
+        }else{
+            fraseFormat.setText(frase);
+            System.out.println("done "+frase);
+        }
+
+
+
     }
 }
